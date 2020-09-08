d2bfbf58a03c9e757ba8c08f0ef0769fc981e980
Elkjøp Låne-PC <Elkjp Lne-PC@158.37.234.243>
2020-09-08 13:42:45 +0200

    Lab2

--- dat100Lab2/src/dat100Lab2/oppgave7a.java
+++ dat100Lab2/src/dat100Lab2/oppgave7a.java
@@ -8,26 +8,28 @@ public class oppgave7a {
 		
 	
 		System.out.println("Poeng      Karakter");
+		
 		for(int i=0;i<10;i++) {
-			int a=Integer.parseInt(showInputDialog("enter first number: "));
 			
-		
-		
-		if (0>a || a>100) {
-			System.out.println("tallet skal være mellom 0 og 100!!!");
-		}else if (0<=a && a<40) { 
-			System.out.println(a+"              F");
-	    }else if (40<=a && a<50) {
-			System.out.println(a+"              E");
-	    }else if (50<=a && a<60) {
-			System.out.println(a+"              D");
-	    }else if (60<=a && a<80) {
-			System.out.println(a+"              C");
-	    }else if (80<=a && a<90) {
-			System.out.println(a+"              B");
-	    }else if (90<=a && a<=100) {
-			System.out.println(a+"              A");
-	    }
-	  }
+			int a=Integer.parseInt(showInputDialog("enter first number: "));
+				
+			if (0>a || a>100) {
+				a=Integer.parseInt(showInputDialog("enter  anumber between 0 and 100"));
+				i-=1;
+				
+			}else if (0<=a && a<40) { 
+				System.out.println(a+"              F");
+		    }else if (40<=a && a<50) {
+				System.out.println(a+"              E");
+		    }else if (50<=a && a<60) {
+				System.out.println(a+"              D");
+		    }else if (60<=a && a<80) {
+				System.out.println(a+"              C");
+		    }else if (80<=a && a<90) {
+				System.out.println(a+"              B");
+		    }else if (90<=a && a<=100) {
+				System.out.println(a+"              A");
+		    }
+		}
 	}
 }
